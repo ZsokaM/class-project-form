f0fc2a53e11169e603fba8151cba82dadd4ff553
ZsokaM <58025019+ZsokaM@users.noreply.github.com>
2021-07-26 17:10:13 +0200

    Update Context.tsx

--- frontend/src/components/Context.tsx
+++ frontend/src/components/Context.tsx
@@ -5,6 +5,7 @@ import React, {
   useState,
 } from "react";
 import Axios, { AxiosResponse } from "axios";
+import { AxiosUserApi } from "../utils/AxiosApis";
 import { UserInterface } from "../utils/Interfaces";
 
 export const myContext = createContext<Partial<UserInterface>>({});
@@ -13,7 +14,7 @@ export default function Context(props: PropsWithChildren<any>) {
   const [user, setUser] = useState<UserInterface>();
 
   useEffect(() => {
-    Axios.get("http://localhost:5050/user", { withCredentials: true }).then(
+    AxiosUserApi.get("user", { withCredentials: true }).then(
       (res: AxiosResponse) => {
         setUser(res.data);
       }
