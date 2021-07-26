dfc709c299f0408c2a440b29bdd4150031535f51
ZsokaM <58025019+ZsokaM@users.noreply.github.com>
2021-07-26 17:24:06 +0200

    Update Signup.tsx

--- frontend/src/Pages/Signup.tsx
+++ frontend/src/Pages/Signup.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
-import axios, { AxiosResponse } from "axios";
+import { AxiosResponse } from "axios";
 import Fieldset from "../components/Form/Fieldset";
 import FormInput from "../components/Form/FormInput";
 import Button from "../components/Button";
+import { AxiosUserApi } from "../utils/AxiosApis";
 
 export default function Signup() {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
   const signup = () => {
-    axios
-      .post(
-        "http://localhost:5050/signup",
+    AxiosUserApi.post(
+      "signup",
         {
           username,
           password,
