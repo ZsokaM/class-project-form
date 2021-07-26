5ffff15bc48bbc091d98ec57ee721ce01b7d9daa
ZsokaM <58025019+ZsokaM@users.noreply.github.com>
2021-07-26 15:52:08 +0200

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -95,8 +95,8 @@ passport.deserializeUser((id: string, cb) => {
 	});
 });
 
-app.use("/", complaintRouter);
-app.use("/", userRouter);
+app.use("/api/complaint", complaintRouter);
+app.use("/api/user", userRouter);
 
 app.listen(process.env.PORT, () => {
 	console.log(`Listening on http://localhost:${process.env.PORT}`);
