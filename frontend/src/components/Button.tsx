8f9a35ccfa2e8ec58888b5a46a3934c8211d9f9c
ZsokaM <58025019+ZsokaM@users.noreply.github.com>
2021-07-26 17:20:49 +0200

    Update Button.tsx

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 interface Ibutton {
   btnType?: "primary" | "secondary" | "simple";
   type?: "button" | "submit";
-  onClick?: any;
+  onClick?: ()=>void;
   disabled?: boolean;
 }
 
